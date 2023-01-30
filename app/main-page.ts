cc02fb04ad7fcd1cb008dacd0db0d05839fd9187
Aleksey <alexeipoyarkin@gmail.com>
2023-01-31 01:58:24 +0500

    Layout

--- app/main-page.ts
+++ app/main-page.ts
@@ -3,5 +3,4 @@ import { HelloWorldModel } from './main-view-model'
 
 export function navigatingTo(args: EventData) {
   const page = <Page>args.object
-  page.bindingContext = new HelloWorldModel()
 }
